0c12922e9731c6eb3c354d570e2787f8ba05a871
YnsHM12 <ynshm1998@gmail.com>
2025-05-08 21:14:36 +0100

    Separated all menu logic and method handling into TechCompanyService class

--- src/CA_2/YnshmTech.java
+++ src/CA_2/YnshmTech.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package CA_2;
-
+import java.util.Scanner;
 /**
  *
  * @author Marwan
@@ -17,7 +17,10 @@ public class YnshmTech {
         SORT, SEARCH, ADD_RECORDS, GENERATE_RANDOM, EXIT
     }
     public static void main(String[] args) {
-        
+         Scanner sc = new Scanner(System.in); // Scanner for input
+        boolean running = true; // Loop control
+        TechCompanyService service = new TechCompanyService(); // Logic handler
+
     }
     
 }
